c111afe09188f84bf94e0cd48858ef82b7535453
Pamela1607 <126475858+Pamela1607@users.noreply.github.com>
2023-03-01 14:16:38 -0300

    Add files via upload

---
+++ Medidas.java
@@ -0,0 +1,40 @@
+	import java.util.Locale;
+	import java.util.Scanner;
+	
+	public class Medidas {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner (System.in);
+		
+		double a,b,c, quadrado, triangulo,trapezio;
+		
+		System.out.print("Digite a medida A: ");
+		a = sc.nextDouble();
+		
+		System.out.print("Digite a medida B: ");
+		b = sc.nextDouble();
+		
+		System.out.print("Digite a medida C: ");
+		c = sc.nextDouble();
+		
+		quadrado = a * a;
+		System.out.println();
+		System.out.printf("AREA DO QUADRADO = %.4f", quadrado);
+		
+		triangulo = (a * b) / 2;
+		System.out.println();
+		System.out.printf("AREA DO TRIANGULO = %.4f", triangulo);
+		
+		trapezio = ((a + b) * c) / 2;
+		System.out.println();
+		System.out.printf("AREA DO TRAPEZIO = %.4f", trapezio);
+		
+		sc.close();
+		
+		
+
+	}
+
+}
