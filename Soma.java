fd48db8f441e2d599bc36dc110682955b4618a35
Pamela1607 <126475858+Pamela1607@users.noreply.github.com>
2023-03-01 11:17:31 -0300

    Add files via upload

---
+++ Soma.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+	public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner (System.in);
+		int A, B , Soma;
+		A = sc.nextInt();
+		B = sc.nextInt ();
+		Soma = A + B;
+		System.out.println("Soma =" + Soma);
+		sc.close();
+		
+	}
+
+}
